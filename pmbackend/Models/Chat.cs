7c86f42e7de95f3d065d46a1554175902e1cda70
Dogukan-lab <57491366+Dogukan-lab@users.noreply.github.com>
2024-03-19 13:14:17 +0100

    [ADD] Started working on chat data

---
+++ pmbackend/Models/Chat.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace pmbackend.Models;
+
+public class Chat
+{
+    [Key]
+    public int Id; 
+    public bool IsVisible { get; set; }
+    public ICollection<PmUser>? Users { get; set; }
+    public ICollection<Message>? Messages { get; set; }
+}
---
+++ pmbackend/Models/Message.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace pmbackend.Models;
+
+public class Message
+{
+   [Key]
+   public int Id;
+   public int UserId { get; set; }
+   public int ChatId { get; set; }
+   public string Data { get; set; } = string.Empty;
+   public DateTime TimeStamp { get; set; }
+}
